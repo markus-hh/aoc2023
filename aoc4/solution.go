c6f28cf64f0062848661e1b77531ad6ffe8b6e05
Markus Richter <markus.richter@3m5.de>
2023-12-04 14:35:01 +0100

    add day 4 part 2 solution

--- aoc4/solution.go
+++ aoc4/solution.go
@@ -39,6 +39,18 @@ func determineCardValue(scratchCard ScratchCard) (cardValue int) {
 	return
 }
 
+func determineMatchNumber(scratchCard ScratchCard) (numberOfMatches int) {
+	for _, chosenNumber := range util.RemoveDuplicates[int](scratchCard.chosenNumbers) {
+		isWinningNumber := slices.Contains(scratchCard.winningNumbers, chosenNumber)
+
+		if isWinningNumber {
+			numberOfMatches++
+		}
+	}
+
+	return
+}
+
 func parseScratchCard(line string) (card ScratchCard) {
 	_, numbersRaw, _ := strings.Cut(line, ": ")
 	splitNumbersRaw := strings.Split(numbersRaw, " | ")
@@ -70,9 +82,34 @@ func solvePart1(input string) {
 	fmt.Println(sum)
 }
 
+func solvePart2(input string) {
+	lines := util.SplitLines(input)
+	scratchCards := parseScratchCards(lines)
+
+	var scratchCardAmounts = make([]int, len(scratchCards))
+	for index := 0; index < len(scratchCards); index++ {
+		scratchCardAmounts[index] = 1
+	}
+
+	sum := 0
+	for index := 0; index < len(scratchCards); index++ {
+		scratchCard := scratchCards[index]
+		scratchCardAmount := scratchCardAmounts[index]
+		sum += scratchCardAmount
+
+		matchNumber := determineMatchNumber(scratchCard)
+
+		for lookahead := 1; lookahead <= matchNumber; lookahead++ {
+			scratchCardAmounts[index + lookahead] += scratchCardAmount
+		}
+	}
+
+	fmt.Println(sum)
+}
+
 func main() {
 	input := util.FetchInput(4)
 
-	solvePart1(input)
+	solvePart2(input)
 	// fmt.Println(input)
 }
