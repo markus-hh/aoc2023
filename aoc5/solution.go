f5a5fd94fce362f25667a090436720a808006510
Markus Richter <markus.richter@3m5.de>
2023-12-05 14:01:12 +0100

    add day 5 part 2 solution

--- aoc5/solution.go
+++ aoc5/solution.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -9,27 +11,112 @@ import (
 )
 
 type MapRange struct {
-	sourceStart int
+	sourceStart      int
 	destinationStart int
-	length int
+	length           int
 }
 
 func determineMappedIndex(category []MapRange, index int) int {
 	for _, mapRange := range category {
-		if index >= mapRange.sourceStart && index < mapRange.sourceStart + mapRange.length {
-			offset := mapRange.destinationStart - mapRange.sourceStart
-			return index + offset
+		if index >= mapRange.sourceStart && index < mapRange.sourceStart+mapRange.length {
+			return mapIndex(mapRange, index)
 		}
 	}
 
 	return index
 }
 
+func mapIndex(mapRange MapRange, index int) int {
+	offset := mapRange.destinationStart - mapRange.sourceStart
+	return index + offset
+}
+
 func determineMappedIndexAllCategories(categories [][]MapRange, index int) int {
-	for categoryIndex := 0; categoryIndex < len(categories) - 1; categoryIndex++ {
+	for categoryIndex := 0; categoryIndex < len(categories)-1; categoryIndex++ {
 		index = determineMappedIndex(categories[categoryIndex], index)
 	}
 	return index
+}
+
+func findMappedMapRangesAllCategories(sortedCategories [][]MapRange, mapRange MapRange) []MapRange {
+	mapRanges := []MapRange{mapRange}
+	for _, sortedCategory := range sortedCategories {
+		mapRanges = findMappedRanges(sortedCategory, mapRanges)
+	}
+
+	return mapRanges
+}
+
+func findMappedRanges(sortedCategory []MapRange, mapRanges []MapRange) (totalMapRanges []MapRange) {
+	for _, mapRange := range mapRanges {
+		currentMappedMapRanges := findMappedRange(sortedCategory, mapRange)
+		for _, mappedMapRange := range currentMappedMapRanges {
+			totalMapRanges = append(totalMapRanges, mappedMapRange)
+		}
+	}
+
+	return
+}
+
+func findMappedRange(sortedCategory []MapRange, mapRange MapRange) (mappedMapRanges []MapRange) {
+	firstIndex := mapRange.destinationStart
+	lastIndex := mapRange.destinationStart + mapRange.length - 1
+
+	firstCategoryMapRange := sortedCategory[0]
+
+	for currentIndex := firstIndex; currentIndex <= lastIndex; currentIndex++ {
+		var endIndex int
+		var mappedIndex int
+		var length int
+
+		if currentIndex < firstCategoryMapRange.sourceStart {
+			endIndex = min(firstCategoryMapRange.sourceStart-1, lastIndex)
+			length = endIndex - currentIndex + 1
+			mappedIndex = currentIndex
+		} else {
+			rangeBeforeIndex := findRangeIndexBefore(sortedCategory, currentIndex)
+			rangeBefore := sortedCategory[rangeBeforeIndex]
+
+			rangeHit := currentIndex < rangeBefore.sourceStart+rangeBefore.length
+			if rangeHit {
+				// range hit
+				endIndex = min(rangeBefore.sourceStart+rangeBefore.length-1, lastIndex)
+				mappedIndex = mapIndex(rangeBefore, currentIndex)
+				length = endIndex - currentIndex + 1
+			} else {
+				// range miss
+				if rangeBeforeIndex == len(sortedCategory)-1 {
+					endIndex = lastIndex
+				} else {
+					endIndex = min(lastIndex, sortedCategory[rangeBeforeIndex + 1].sourceStart-1)
+				}
+
+				length = endIndex - currentIndex + 1
+				mappedIndex = currentIndex
+			}
+		}
+
+		mappedMapRange := MapRange{
+			sourceStart:      currentIndex,
+			destinationStart: mappedIndex,
+			length:           length,
+		}
+
+		mappedMapRanges = append(mappedMapRanges, mappedMapRange)
+		currentIndex = endIndex
+	}
+
+	return
+}
+
+func findRangeIndexBefore(sortedCategory []MapRange, targetIndex int) int {
+	for rangeIndex := 0; rangeIndex < len(sortedCategory); rangeIndex++ {
+		if sortedCategory[rangeIndex].sourceStart > targetIndex {
+			return rangeIndex - 1
+		}
+	}
+
+	return len(sortedCategory) - 1
 }
 
 func extractSeedNumbers(line string) (seedNumbers []int) {
@@ -49,25 +136,33 @@ func extractMapRange(line string) (mapRange MapRange) {
 	return
 }
 
-func extractCategory(lines []string, startIndex int) (mapRanges []MapRange) {
-	for offset := 1; startIndex + offset < len(lines); offset++ {
-		line := lines[startIndex + offset]
+func extractCategory(lines []string, startIndex int) (category []MapRange) {
+	for offset := 1; startIndex+offset < len(lines); offset++ {
+		line := lines[startIndex+offset]
 		if line == "" {
 			break
 		}
 
 		mapRange := extractMapRange(line)
-		mapRanges = append(mapRanges, mapRange)
+		category = append(category, mapRange)
 	}
 
 	return
 }
 
+func sortCategory(category []MapRange) []MapRange {
+	slices.SortFunc(category,
+		func(a, b MapRange) int {
+			return cmp.Compare(a.sourceStart, b.sourceStart)
+		})
+	return category
+}
+
 func extractCategories(lines []string) (categories [][]MapRange) {
 	for index := 0; index < len(lines); index++ {
 		line := lines[index]
-		if(line == "") {
-			category := extractCategory(lines, index + 1)
+		if line == "" && index != len(lines) - 1 {
+			category := extractCategory(lines, index+1)
 			categories = append(categories, category)
 		}
 	}
@@ -90,7 +185,46 @@ func solvePart1(input string) {
 	fmt.Println(smallestLocation)
 }
 
+func solvePart2(input string) {
+	lines := strings.Split(input, "\n")
+	seedNumbers := extractSeedNumbers(lines[0])
+
+	categoryLines := lines[1:]
+	categories := extractCategories(categoryLines)
+
+	for index := 0; index < len(categories); index++ {
+		categories[index] = sortCategory(categories[index])
+	}
+
+	sortedCategories := categories
+	var totalMappedRanges []MapRange
+
+	for seedIndex := 0; 2*seedIndex < len(seedNumbers); seedIndex++ {
+		seedNumberStart := seedNumbers[2*seedIndex]
+		seedNumberLength := seedNumbers[2*seedIndex+1]
+
+		mapRange := MapRange {
+			sourceStart: seedNumberStart,
+			destinationStart: seedNumberStart,
+			length: seedNumberLength,
+		}
+		mappedMapRanges := findMappedMapRangesAllCategories(sortedCategories, mapRange)
+		
+		for _, mappedMapRange := range mappedMapRanges {
+			totalMappedRanges = append(totalMappedRanges, mappedMapRange)
+		}
+	}
+
+	smallestLocation := totalMappedRanges[0].destinationStart
+	for index := 1; index < len(totalMappedRanges); index++ {
+		mappedMapRange := totalMappedRanges[index]
+		smallestLocation = min(smallestLocation, mappedMapRange.destinationStart)
+	}
+
+	fmt.Println(smallestLocation)
+}
+
 func main() {
 	input := util.FetchInput(5)
-	solvePart1(input)
+	solvePart2(input)
 }
