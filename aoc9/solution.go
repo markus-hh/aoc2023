a48a90a8ea112d31412f9ecec1c05fe54f8c6d7f
Markus Richter <markus.richter@3m5.de>
2023-12-09 17:04:14 +0100

    add day 9 part 2 solution

--- aoc9/solution.go
+++ aoc9/solution.go
@@ -35,6 +35,40 @@ func predictNextNumber(numbers []int) int {
 	return nextNumberAggregate
 }
 
+func predictPreviousNumber(numbers []int) int {
+	previousNumberAggregate := numbers[0]
+
+	subtractNextNumber := true
+	for {
+		if len(numbers) == 1 {
+			previousNumberAggregate += numbers[0]
+			break
+		}
+
+		numbersNextLine := make([]int, len(numbers)-1)
+		for index := 0; index < len(numbersNextLine); index++ {
+			delta := numbers[index+1] - numbers[index]
+			numbersNextLine[index] = delta
+		}
+
+		numbers = numbersNextLine
+		finished := !util.Any(numbers, isNotZero)
+
+		if finished {
+			break
+		}
+
+		if subtractNextNumber {
+			previousNumberAggregate -= numbers[0]
+		} else {
+			previousNumberAggregate += numbers[0]
+		}
+		subtractNextNumber = !subtractNextNumber
+	}
+
+	return previousNumberAggregate
+}
+
 func isNotZero(number int) bool {
 	return number != 0
 }
@@ -42,6 +76,19 @@ func isNotZero(number int) bool {
 func extractNumbersFromLine(line string) []int {
 	numberStrings := strings.Split(line, " ")
 	return util.MapFunc(numberStrings, util.AtoiUnsafe)
+}
+
+func solvePart2(input string) {
+	lines := util.SplitLines(input)
+
+	sum := 0
+	for _, line := range lines {
+		numbers := extractNumbersFromLine(line)
+		predictedNumber := predictPreviousNumber(numbers)
+		sum += predictedNumber
+	}
+
+	fmt.Println(sum)
 }
 
 func solvePart1(input string) {
@@ -59,5 +106,5 @@ func solvePart1(input string) {
 
 func main() {
 	input := util.FetchInput(9)
-	solvePart1(input)
+	solvePart2(input)
 }
